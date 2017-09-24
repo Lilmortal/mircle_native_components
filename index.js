68b684452e07eb7937d4b08abd8fe1feba25eb4b
Jack Tan <jacktan165@gmail.com>
2017-09-24 19:29:40 +1300

    Added index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+import BackgroundImage from "./backgroundImage";
+import Button from "./button";
+import Camera from "./camera";
+import DatePicker from "./datePicker";
+import DrawerPanel from "./drawerPanel";
+import Feed from "./feed";
+import Hamburger from "./hamburger";
+import ListItem from "./listItem";
+import Modal from "./modal";
+import Picker from "./picker";
+import ProfileImage from "./profileImage";
+import Spinner from "./spinner";
+import Text from "./text";
+import TextInput from "./textInput";
+
+export {
+  BackgroundImage,
+  Button,
+  Camera,
+  DatePicker,
+  DrawerPanel,
+  Feed,
+  Hamburger,
+  ListItem,
+  Modal,
+  Picker,
+  ProfileImage,
+  Spinner,
+  Text,
+  TextInput
+};
